82869c6bef368e416570efd3b757676b578efa12
renklus <renklus@users.noreply.github.com>
2020-12-02 02:20:30 +0100

    project creation

---
+++ d1p1a/d1p1a/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace d1p1a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
