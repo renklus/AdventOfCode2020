469f1fa2bb11b9bc2001f7791fe571ea5b41b18c
renklus <renklus@users.noreply.github.com>
2020-12-06 16:09:09 +0100

    added project template

---
+++ d1p1a/template/Program.cs
@@ -0,0 +1,42 @@
+﻿#region Usings
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace template
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var result = 0;
+            
+            using (var fs = new FileStream(
+                "input.txt",
+                FileMode.Open,
+                FileAccess.Read,
+                FileShare.Read))
+            {
+                using (TextReader tr = new StreamReader(fs))
+                {
+                    var line = await tr.ReadLineAsync().ConfigureAwait(false);
+
+                    if (line == null)
+                        throw new Exception();
+
+                    do
+                    {
+                        
+                        
+                        line = await tr.ReadLineAsync().ConfigureAwait(false);
+                    } while (line != null);
+                }
+            }
+
+            Console.Out.WriteLine(result);
+        }
+    }
+}
