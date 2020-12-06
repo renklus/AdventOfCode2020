73bd35fafea6ae9d300883ffd703ff594843c330
renklus <renklus@users.noreply.github.com>
2020-12-06 11:44:45 +0100

    d4 was d5

--- d1p1a/d4p2a/Program.cs
+++ d1p1a/d4p2a/Program.cs
@@ -19,9 +19,6 @@ namespace d4p2a
                 FileAccess.Read,
                 FileShare.Read))
             {
-                var highestSeatId = 0;
-                var seatIDs = new bool[971];
-
                 using (TextReader tr = new StreamReader(fs))
                 {
                     var line = await tr.ReadLineAsync().ConfigureAwait(false);
@@ -29,33 +26,15 @@ namespace d4p2a
                     if (line == null)
                         throw new Exception();
 
-
                     do
                     {
-                        var rowData = line[0 .. 7];
-                        var columnData = line[7 .. 10];
-
-                        var rowBinary = string.Concat(rowData.Select(i => i == 'B' ? '1' : '0'));
-                        var columnBinary =
-                            string.Concat(columnData.Select(i => i == 'R' ? '1' : '0'));
-                        var row = Convert.ToInt32(rowBinary, 2);
-                        var column = Convert.ToInt32(columnBinary, 2);
-
-                        var seatId = row * 8 + column;
-                        if (seatId > highestSeatId)
-                            highestSeatId = seatId;
-
-                        seatIDs[seatId] = true;
-
+                        
+                        
                         line = await tr.ReadLineAsync().ConfigureAwait(false);
                     } while (line != null);
                 }
 
-                for (var i = 0; i < seatIDs.Length-1; i++)
-                {
-                    if (seatIDs[i + 1] && seatIDs[i - 1] && !seatIDs[i])
-                        Console.Out.WriteLine(i);
-                }
+                Console.Out.WriteLine();
             }
         }
     }
---
+++ d1p1a/d5p2a/Program.cs
@@ -0,0 +1,62 @@
+﻿#region Usings
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace d5p2a
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            using (var fs = new FileStream(
+                "input.txt",
+                FileMode.Open,
+                FileAccess.Read,
+                FileShare.Read))
+            {
+                var highestSeatId = 0;
+                var seatIDs = new bool[971];
+
+                using (TextReader tr = new StreamReader(fs))
+                {
+                    var line = await tr.ReadLineAsync().ConfigureAwait(false);
+
+                    if (line == null)
+                        throw new Exception();
+
+
+                    do
+                    {
+                        var rowData = line[0 .. 7];
+                        var columnData = line[7 .. 10];
+
+                        var rowBinary = string.Concat(rowData.Select(i => i == 'B' ? '1' : '0'));
+                        var columnBinary =
+                            string.Concat(columnData.Select(i => i == 'R' ? '1' : '0'));
+                        var row = Convert.ToInt32(rowBinary, 2);
+                        var column = Convert.ToInt32(columnBinary, 2);
+
+                        var seatId = row * 8 + column;
+                        if (seatId > highestSeatId)
+                            highestSeatId = seatId;
+
+                        seatIDs[seatId] = true;
+
+                        line = await tr.ReadLineAsync().ConfigureAwait(false);
+                    } while (line != null);
+                }
+
+                for (var i = 0; i < seatIDs.Length-1; i++)
+                {
+                    if (seatIDs[i + 1] && seatIDs[i - 1] && !seatIDs[i])
+                        Console.Out.WriteLine(i);
+                }
+            }
+        }
+    }
+}
